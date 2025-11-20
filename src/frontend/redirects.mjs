7214f416f0ecfd7c26aa1a4a0ad43f1472da8528
David Pine <david.pine@microsoft.com>
2025-11-20 14:59:49 -0600

    fix: update package dependencies and correct redirect paths

--- src/frontend/redirects.mjs
+++ src/frontend/redirects.mjs
@@ -1,6 +1,8 @@
 export const redirects = {
-    // Docs redirects
+    // https://docs.astro.build/en/guides/routing/#configured-redirects
+    // For example:
+    // '/original/path/': '/new/path'
     '/get-started/welcome/': '/docs/',
-    '/integrations/postgres': '/integrations/databases/postgres/',
+    '/integrations/postgres/': '/integrations/databases/postgres/',
     '/integrations/rabbitmq/': '/integrations/messaging/rabbitmq/',
 };
