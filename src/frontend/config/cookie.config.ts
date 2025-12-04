0cfc53b992dfc81ff69d87e9ef42009122effe63
David Pine <david.pine@microsoft.com>
2025-12-04 15:26:58 -0600

    Add social links configuration, cookie SVG icon, and custom styles for cookie consent
    - Created a new social configuration file with links to GitHub, Discord, X, BlueSky, YouTube, and Twitch.
    - Added a new SVG icon for cookies.
    - Implemented custom CSS styles for cookie consent, supporting both light and dark themes with appropriate variables for colors and styles.

---
+++ src/frontend/config/cookie.config.ts
@@ -0,0 +1,69 @@
+import type { CookieConsentConfig } from "@jop-software/astro-cookieconsent";
+
+export const cookieConfig: CookieConsentConfig = {
+    guiOptions: {
+        consentModal: {
+            layout: 'box',
+            position: 'bottom right'
+        },
+        preferencesModal: {
+            layout: "box"
+        },
+    },
+    categories: {
+        necessary: {
+            enabled: true,
+            readOnly: true
+        },
+        analytics: {
+            enabled: true,
+            readOnly: false
+        },
+        advertising: {
+            enabled: true,
+            readOnly: false
+        }
+    },
+    language: {
+        default: 'en',
+        translations: {
+            en: {
+                consentModal: {
+                    title: 'We use cookies',
+                    description: 'Cookie modal description',
+                    acceptAllBtn: 'Accept all',
+                    acceptNecessaryBtn: 'Reject all',
+                    showPreferencesBtn: 'Manage Individual preferences'
+                },
+                preferencesModal: {
+                    title: 'Manage cookie preferences',
+                    acceptAllBtn: 'Accept all',
+                    acceptNecessaryBtn: 'Reject all',
+                    savePreferencesBtn: 'Accept current selection',
+                    closeIconLabel: 'Close modal',
+                    sections: [
+                        {
+                            title: 'Somebody said ... cookies?',
+                            description: 'I want one!'
+                        },
+                        {
+                            title: 'Strictly Necessary cookies',
+                            description: 'These cookies are essential for the proper functioning of the website and cannot be disabled.',
+                            //this field will generate a toggle linked to the 'necessary' category            
+                            linkedCategory: 'necessary'
+                        },
+                        {
+                            title: 'Performance and Analytics',
+                            description: 'These cookies collect information about how you use our website. All of the data is anonymized and cannot be used to identify you.',
+                            linkedCategory: 'analytics'
+                        },
+                        {
+                            title: 'More information',
+                            description: 'For any queries in relation to my policy on cookies and your choices, please <a href="https://support.microsoft.com/en-US/contactus/">contact us</a>'
+                        }
+                    ]
+                }
+            }
+        }
+    }
+};
